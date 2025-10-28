43a800a6a8197310dcb0ef005a8d0dc7d4485827
KibaKizu <leeyihang911@gmail.com>
2025-10-28 20:51:24 +0800

    added "recent activity" and "My subjects" to tc dashboard

--- TeacherDashboard.aspx.designer.cs
+++ TeacherDashboard.aspx.designer.cs
@@ -58,5 +58,23 @@ namespace BrainBlitz
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblAverageScore;
+
+        /// <summary>
+        /// rptSubjects control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptSubjects;
+
+        /// <summary>
+        /// rptRecentActivity control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptRecentActivity;
     }
 }
